1c3c243cce4739adfa2999d84869bb84a20bb341
NicholasOkoth38 <nicholas.okoth@student.moringaschool.com>
2021-08-26 11:26:02 +0300

    initial one

---
+++ start.sh
@@ -0,0 +1,7 @@
+export MOVIE_API_KEY='8cc08b9703381811723bfa27eafbb000'
+export SECRET_KEY='moringa4'
+export MAIL_USERNAME='nicholasko28@gmail.com'
+export MAIL_PASSWORD='successtwo2'
+
+python3 manage.py server
+
